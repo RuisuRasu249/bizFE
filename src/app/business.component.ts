ce6661bfe4b4b4cbadc738138e19699ad282dba6
Luis Ras <Ras-L@ulster.ac.uk>
2024-11-25 19:55:02 +0000

    Data Visualisation Generating placeholder loremIpsum content Building Album page layout

--- src/app/business.component.ts
+++ src/app/business.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { DataService } from './data.service';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'business',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   providers: [DataService],
   templateUrl: './business.component.html',
   styleUrl: './business.component.css'
@@ -12,6 +12,8 @@ import { DataService } from './data.service';
 
 export class BusinessComponent {
   business_list: any;
+  loremIpsum: any;
+  
 
   constructor(public dataService: DataService, private route: ActivatedRoute){}
 
@@ -19,5 +21,18 @@ export class BusinessComponent {
     this.business_list = this.dataService.getBusiness(
       this.route.snapshot.paramMap.get('id')
     )
+
+    this.dataService.getLoremIpsum(1)
+    .subscribe((response: any) => {
+      this.loremIpsum = response.text.slice(0, 400);
+    })
+  }
+
+  getAverageRating(reviews: any[]) {
+    if (!reviews || reviews.length === 0) {
+      return []; // No reviews, no stars
+    }
+    const averageRating = reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length;
+    return Array(Math.round(averageRating)).fill(0); // Create an array for stars
   }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,16 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import jsonData from '../assets/albumdb.albums.json'
 
+@Injectable({
+    providedIn: 'root'
+})
 export class DataService{
 
     pageSize: number = 3;
+
+    constructor(private http: HttpClient){}
 
     getBusinesses(page: number){
         let pageStart = (page - 1) * this.pageSize;
@@ -22,5 +30,14 @@ export class DataService{
             }
         })
         return dataToReturn;
+    }
+
+    getLoremIpsum(paragraphs: number): Observable<any>{
+
+        let API_key = 'OTICg67nm4BTJAj3FaYxKg==w8YD3uEtrTX2s8FV';
+        return this.http.get<any>(
+            'https://api.api-ninjas.com/v1/loremipsum?paragraphs=' + paragraphs,
+            {headers: {'X-Api-Key': API_key}}
+        );
     }
 }
